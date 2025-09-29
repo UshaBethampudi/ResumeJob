1e66467f303a0575fb1e392f64f0fb4e476e3a56
SrivastavaRachit <rachitsrivastava789@gmail.com>
2025-09-29 21:09:21 +0530

    ffrontend url added

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000', // Your backend API URL
+  baseURL: 'https://resume-job-backend-hwls.onrender.com', // Your backend API URL
   withCredentials: true,
 });
 
